d121e90d9977628be5beac77190be4e2bf06eaed
jokulyao <forlive_yxs@qq.com>
2015-09-24 12:02:56 +0800

    删除无用的项目

--- JavaPro1/com/nstc/Test.java
+++
@@ -1,5 +0,0 @@
-package com.nstc;
-
-public class Test {
-
-}
--- JavaPro3/src/com/nstc/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.nstc;
-
-public class HelloWorld {
-
-	public static void main(String[] args){
-		String str = new String("Test");
-		
-	}
-}
