17132818a530b22eaf75c590e009af8374626400
jokulyao <forlive_yxs@qq.com>
2015-10-08 15:32:22 +0800

    创建容器项目

---
+++ ContainerPro/src/com/nstc/container/EnhancedFor.java
@@ -0,0 +1,20 @@
+package com.nstc.container;
+
+import java.util.*;
+
+public class EnhancedFor {
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+		for(int i : arr) {
+			System.out.println(i);
+		}
+		
+		Collection<String> c = new ArrayList<String>();
+		c.add(new String("aaa"));
+		c.add(new String("bbb"));
+		c.add(new String("ccc"));
+		for(Object o : c) {
+			System.out.println(o);
+		}
+	}
+}
