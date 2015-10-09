a8c029ef7f77793e6b6a21bea117e6604afd8788
jokulyao <forlive_yxs@qq.com>
2015-10-09 08:59:41 +0800

    修改

--- ContainerPro/src/com/nstc/container/FinalMapTest.java
+++ ContainerPro/src/com/nstc/container/FinalMapTest.java
@@ -3,6 +3,7 @@ package com.nstc.container;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class FinalMapTest {
 	public static void main(String[] args){
@@ -13,22 +14,27 @@ public class FinalMapTest {
 		for(Iterator<Map.Entry<String, String>> it = map.entrySet().iterator(); it.hasNext(); ){
 			System.out.println(it.next().getValue());
 		}
-		System.out.println();
+		System.out.println("1");
 		for(Map.Entry<String, String> entry : map.entrySet()){
 			System.out.println(entry.getValue());
 		}
-		System.out.println();
+		System.out.println("2");
 		for(String v : map.values()){
 			System.out.println(v);
 		}
-		System.out.println();
+		System.out.println("3");
 		Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
 		while(it.hasNext()){
 			System.out.println(it.next().getValue());
 		}
-		System.out.println();
+		System.out.println("4");
 		for(String key : map.keySet()){
 			System.out.println(map.get(key));
+		}
+		System.out.println("5");
+		Set<Map.Entry<String, String>> set = map.entrySet();
+		for(Iterator<Map.Entry<String, String>> it2 = set.iterator(); it2.hasNext(); ){
+			System.out.println(it2.next().getValue());
 		}
 	}
 }
