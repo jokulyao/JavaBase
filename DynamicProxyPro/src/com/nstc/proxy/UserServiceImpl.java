9ac942ff318da93c8116befbaba3fdd7357d84e4
jokulyao <forlive_yxs@qq.com>
2015-10-12 18:06:00 +0800

    创建动态代理项目

---
+++ DynamicProxyPro/src/com/nstc/proxy/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.nstc.proxy;
+/** 
+ * 目标对象
+ * @since 2012-8-9 
+ */  
+public class UserServiceImpl implements UserService {  
+  
+    /* (non-Javadoc) 
+     * @see dynamic.proxy.UserService#add() 
+     */  
+    public void add() {  
+        System.out.println("--------------------add---------------");  
+    }  
+}  
