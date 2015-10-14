23d2b7958cd5e602485d59e81bf8b5c1f80f74f0
jokulyao <forlive_yxs@qq.com>
2015-10-14 15:56:38 +0800

    更新项目

--- DesignPatternsPro/src/com/nstc/pattern/create/singleton/SingletonOther.java
+++ DesignPatternsPro/src/com/nstc/pattern/create/singleton/SingletonOther.java
@@ -2,7 +2,7 @@ package com.nstc.pattern.create.singleton;
 
 /**
  * 因为我们只需要在创建类的时候进行同步，所以只要将创建和getInstance()分开，单独为创建加synchronized关键字
- * 
+ * 考虑性能的话，整个程序只需创建一次实例，所以性能也不会有什么影响
  * @author yaoxs
  */
 
