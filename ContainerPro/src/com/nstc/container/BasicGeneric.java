d4961c06d32f0b993badc32e23bc2299ab58b065
jokulyao <forlive_yxs@qq.com>
2016-05-19 16:14:16 +0800

    更新项目

--- ContainerPro/src/com/nstc/container/BasicGeneric.java
+++ ContainerPro/src/com/nstc/container/BasicGeneric.java
@@ -8,14 +8,16 @@ public class BasicGeneric {
 		c.add("aaa");
 		c.add("bbb");
 		c.add("ccc");
-		for(int i=0; i<c.size(); i++) {
+		for (int i = 0; i < c.size(); i++) {
 			String s = c.get(i);
 			System.out.println(s);
 		}
-		
+
 		Collection<String> c2 = new HashSet<String>();
-		c2.add("aaa"); c2.add("bbb"); c2.add("ccc");
-		for(Iterator<String> it = c2.iterator(); it.hasNext(); ) {
+		c2.add("aaa");
+		c2.add("bbb");
+		c2.add("ccc");
+		for (Iterator<String> it = c2.iterator(); it.hasNext();) {
 			String s = it.next();
 			System.out.println(s);
 		}
@@ -24,11 +26,13 @@ public class BasicGeneric {
 
 class MyName implements Comparable<MyName> {
 	int age;
-	
+
 	public int compareTo(MyName mn) {
-		if(this.age > mn.age) return 1;
-		else if(this.age < mn.age) return -1;
-		else return 0;
+		if (this.age > mn.age)
+			return 1;
+		else if (this.age < mn.age)
+			return -1;
+		else
+			return 0;
 	}
 }
-
